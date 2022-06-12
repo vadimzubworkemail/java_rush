e177df7e3771ebbc97db13fb9da5e23f08702d17
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-06-12 11:54:06 +0300

    add Task0438

---
+++ src/main/java/ru/javarush/tasks/level4/Task0438.java
@@ -0,0 +1,22 @@
+package ru.javarush.tasks.level4;
+
+/*
+Рисуем линии
+
+Используя цикл for, выведи на экран:
+горизонтальную линию из 10 восьмёрок;
+вертикальную линию из 10 восьмёрок (символ из горизонтальной линии не учитывается).
+*/
+public class Task0438 {
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        for (int i = 1; i <= 10; i++){
+            System.out.print(8);
+        }
+        System.out.println();
+
+        for (int j = 1; j <= 10; j++){
+            System.out.println(8);
+        }
+    }
+}
