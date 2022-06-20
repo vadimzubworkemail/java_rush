195fcd5cb621fa88386ac7f53201c1a1e2ef331b
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-06-20 18:34:14 +0300

    add Task0509

---
+++ src/main/java/ru/javarush/tasks/level5/UdemyArrayHomeWork2.java
@@ -0,0 +1,19 @@
+package ru.javarush.tasks.level5;
+
+/*
+Создайте класс, в котором создайте метод showArray. Инпут параметром данного метода будет двумерный массив
+типа String. Метод должен выводить на экран данный массив в следующем виде:
+{ {элемент00, элемент01}, {элемент10}, {элемент10, элемент11}, }
+Содержимое, естественно будет зависеть от твоего массива.
+Продемонстрируй данный метод.
+ */
+public class UdemyArrayHomeWork2 {
+    public static String[][] showArray(String[][] strings) {
+
+        return strings;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
