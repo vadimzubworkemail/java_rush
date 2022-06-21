7b2b624dc3d6b204c05946919c326a4f99ef3e71
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-06-21 20:18:00 +0300

    add Task0518

---
+++ src/main/java/ru/javarush/tasks/level6/Task0518.java
@@ -0,0 +1,20 @@
+package ru.javarush.tasks.level6;
+
+import java.util.Arrays;
+
+/*
+Сортировка массива
+Реализуй метод main(String[]), который сортирует массив array по возрастанию.
+Для сортировки массива используй метод Arrays.sort(int[]).
+*/
+
+public class Task0518 {
+
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
