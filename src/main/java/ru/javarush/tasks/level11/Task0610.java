d90dda7b2cc5f0307dccf4e1a2ab884eefc9d014
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-07-02 20:35:55 +0300

    add Task0610

---
+++ src/main/java/ru/javarush/tasks/level11/Task0610.java
@@ -0,0 +1,36 @@
+package ru.javarush.tasks.level11;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/*
+Класс ConsoleReader
+*/
+
+public class Task0610 {
+    public static String readString() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return bufferedReader.readLine();
+    }
+
+    public static int readInt() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return Integer.parseInt(bufferedReader.readLine());
+    }
+
+    public static double readDouble() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return Double.parseDouble(bufferedReader.readLine());
+    }
+
+    public static boolean readBoolean() throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        return Boolean.parseBoolean(bufferedReader.readLine());
+    }
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
+
+
