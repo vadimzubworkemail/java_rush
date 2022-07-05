b78c8cdbf80f2b558f72cbe5d6db863ce3b38cee
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-07-05 19:30:41 +0300

    add new classes

---
+++ src/main/java/ru/javarush/tasks/level12/TheLargestNumber.java
@@ -0,0 +1,27 @@
+package ru.javarush.tasks.level12;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+Считай с клавиатуры 5 чисел и добавь в список integers.
+Найди максимальное число в списке.
+Выведи найденное число на экран.
+ */
+public class TheLargestNumber {
+
+    private static final ArrayList<Integer> integers = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < 5; i++) {
+            integers.add(Integer.parseInt(bufferedReader.readLine()));
+        }
+        int max = Integer.MIN_VALUE;
+        for (Integer integer : integers) {
+            max = Math.max(max, integer);
+            System.out.println(max);
+        }
+    }
+}
