b73ec5998a3cfd4600e8fff8a7192f876f381a55
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-07-09 19:58:10 +0300

    add 0713 - 0719

---
+++ src/main/java/ru/javarush/tasks/level13/Task0719.java
@@ -0,0 +1,24 @@
+package ru.javarush.tasks.level13;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+/*
+Вывести числа в обратном порядке
+*/
+
+public class Task0719 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(Integer.parseInt(reader.readLine()));
+        }
+        Collections.reverse(list);
+        list.forEach(System.out::println);
+    }
+}
